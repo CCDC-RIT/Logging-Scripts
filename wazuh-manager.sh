6f94493ba49a0fafd0a10853b0076e214ed6f923
jh6431 <jh6431@rit.edu>
2024-01-18 21:27:23 -0500

    misc blog posts

--- wazuh-manager.sh
+++ wazuh-manager.sh
@@ -22,6 +22,8 @@ cp osquery.conf /etc/osquery/osquery.conf
 cp centralized_agent.conf /var/ossec/etc/shared/agent.conf
 cp internal_options.conf /var/ossec/etc/internal_options.conf
 cp policies/dirtypipe_check.yml /var/ossec/etc/shared/default/dirtypipe_check.yml
-cp policies/detect_linux_keylogger.yml /var/ossec/etc/sca_detect_linux_keylogger.yml
+cp policies/detect_linux_keylogger.yml /var/ossec/etc/shared/default/sca_detect_linux_keylogger.yml
+cp policies/sca_systemfiles.yml /var/ossec/etc/shared/default/sca_systemfiles.yml
 chown wazuh:wazuh /var/ossec/etc/shared/default/dirtypipe_check.yml
 chown wazuh:wazuh /var/ossec/etc/shared/default/sca_detect_linux_keylogger.yml
+chown wazuh:wazuh /var/ossec/etc/shared/default/sca_systemfiles.yml
