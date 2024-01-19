5f76802cf634067505dbc261fb33963812142656
jh6431 <jh6431@rit.edu>
2024-01-19 17:34:22 -0500

    a lot of minor changes

--- wazuh-manager.sh
+++ wazuh-manager.sh
@@ -3,7 +3,7 @@
 
 # change passwords
 curl -so wazuh-passwords-tool.sh https://packages.wazuh.com/4.7/wazuh-passwords-tool.sh
-bash wazuh-passwords-tool.sh -a > /opt/wazuhpw.txt # temporary file name - change later
+bash wazuh-passwords-tool.sh -a > /usr/src/wingstop.txt # temporary file name - change later
 
 # file permissions
 chown root:wazuh /var/ossec/etc/ossec.conf
@@ -17,13 +17,21 @@ chmod -R /var/ossec/etc/rules/ 660
 chmod /var/ossec/etc/internal_options.conf 640
 chmod /var/ossec/logs/ossec.log 660
 
-#TODO: Agent enrollment password, ossec.conf, policies, decoders, osquery 
+#TODO: ossec.conf, policies, decoders, osquery
+
+# create agent groups
+/var/ossec/bin/agent_groups -a -g linux
+/var/ossec/bin/agent_groups -a -g windows
+
+# set up policies and configs
 cp osquery.conf /etc/osquery/osquery.conf
-cp centralized_agent.conf /var/ossec/etc/shared/agent.conf
+cp centralized_agent.conf /var/ossec/etc/linux/agent.conf
 cp internal_options.conf /var/ossec/etc/internal_options.conf
-cp policies/dirtypipe_check.yml /var/ossec/etc/shared/default/dirtypipe_check.yml
-cp policies/detect_linux_keylogger.yml /var/ossec/etc/shared/default/sca_detect_linux_keylogger.yml
-cp policies/sca_systemfiles.yml /var/ossec/etc/shared/default/sca_systemfiles.yml
-chown wazuh:wazuh /var/ossec/etc/shared/default/dirtypipe_check.yml
-chown wazuh:wazuh /var/ossec/etc/shared/default/sca_detect_linux_keylogger.yml
-chown wazuh:wazuh /var/ossec/etc/shared/default/sca_systemfiles.yml
+cp decoders/decoder-linux-sysmon.xml /var/ossec/ruleset/decoders
+cp decoders/docker-decoders.xml /var/ossec/ruleset/decoders
+cp policies/dirtypipe_check.yml /var/ossec/etc/shared/linux/dirtypipe_check.yml
+cp policies/detect_linux_keylogger.yml /var/ossec/etc/shared/linux/sca_detect_linux_keylogger.yml
+cp policies/sca_systemfiles.yml /var/ossec/etc/shared/linux/sca_systemfiles.yml
+chown wazuh:wazuh /var/ossec/etc/shared/linux/dirtypipe_check.yml
+chown wazuh:wazuh /var/ossec/etc/shared/linux/sca_detect_linux_keylogger.yml
+chown wazuh:wazuh /var/ossec/etc/shared/linux/sca_systemfiles.yml
